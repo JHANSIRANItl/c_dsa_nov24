ddf069c9c523c6d0b72da71a10c6a0a9daff6aa7
JHANSIRANItl <jhansiranitl26@gmail.com>
2024-11-11 16:05:20 +0530

    Add files via upload

--- array1.c
+++ array1.c
@@ -1,8 +1,20 @@
 #include<stdio.h>
 int main(){
-    int arr[]={1,2,3};
-    for(int i=0;i<3;i++){
-        printf("%d\n",arr[i]);
+    int n;
+    printf("Enter the size");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the elements");
+    for(int i=0;i<n;i++){
+        scanf("%d\n",&arr[i]);
+    }
+    int temp=arr[n-1];
+    for(int i=n-1;i>0;i--){
+        arr[i]=arr[i-1];
+    }
+    arr[0]=temp;
+    for(int i=0;i<0;i++){
+        printf("%d",arr[i]);
     }
 }
-
+                         

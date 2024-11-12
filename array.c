53ed52fa8c2bafc073b5ce9a33064951814fe55d
JHANSIRANItl <jhansiranitl26@gmail.com>
2024-11-12 16:25:01 +0530

    Add files via upload

--- array.c
+++ array.c
@@ -1,24 +1,20 @@
 #include<stdio.h>
 int main(){
-    int n;
-    printf("Enter the size");
-    scanf("%d",&n);
-    int arr[n];
-    printf("Enter the elements");
-    for(int i=0;i<n;i++){
-        scanf("%d\n",&arr[i]);
-    }
-    int min1=__INT_MAX__;
-    int min2=__INT_MAX__;
-    for(int i=0;i<n;i++){
-        if(arr[i]<min1){
-            min2=min1;
-            min1=arr[i];
-        }
-        else if(arr[i]<min2){
-            min2=arr[i];
+    int r;
+    scanf("%d",&r);
+    int c;
+    scanf("%d",&c);
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
         }
     }
-    printf("The second minimum is:");
-    printf("%d",min2);
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+           printf("%d  ",arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
 }

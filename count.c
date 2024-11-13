d6b98dbf50ebb133bc5e721c1da4cb63aa679c5f
JHANSIRANItl <jhansiranitl26@gmail.com>
2024-11-13 10:52:11 +0530

    Add files via upload

--- count.c
+++ count.c
@@ -1,5 +1,14 @@
-#include<stdio.h>
-int main(){
-    int n=10;
-    printf("%X",n);
+#include <stdio.h>
+
+int main() {
+    int num, count = 0;
+    scanf("%d", &num);
+
+    while (num != 0) {
+        num /= 10;  
+
+    printf("%d", count);
+
+    return 0;
+}
 }
